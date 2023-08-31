6b869212284d80f91bee98131a0da48f3bc531b3
Decker Ayers <ayersdecker@gmail.com>
2023-08-31 16:40:48 -0400

    Initial

---
+++ Audiogram/Audiogram/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Audiogram;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
