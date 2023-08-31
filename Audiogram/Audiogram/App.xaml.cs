cb771df56120079de65bf70a244bae7377cd73b4
Decker Ayers <ayersdecker@gmail.com>
2023-08-31 16:48:54 -0400

    Initial

---
+++ Audiogram/Audiogram/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Audiogram;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Audiogram/Audiogram/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Audiogram;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
